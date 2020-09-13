bcb1532ff0de70db5ca2a9a4ee1499eb9d2d8190
eunjae <swejyong@naver.com>
2020-09-13 23:11:44 +0900

    testpage 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "테스트 글",
+        "excerpt":"안녕하세요  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/",
+        "teaser": null
+      }]

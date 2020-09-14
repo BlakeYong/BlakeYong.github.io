42ea1bdb2f20822ab3bb3d6e630d588562720b60
eunjae <swejyong@naver.com>
2020-09-14 10:21:58 +0900

    테스트 post 삭제

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "테스트 글",
-        "excerpt":"안녕하세요  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/",
-        "teaser": null
-      }]
+var store = []
